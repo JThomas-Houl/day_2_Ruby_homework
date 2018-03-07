36999ec4b04f462a3658caf73f17fa009148b70e
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-03-07 10:26:13 +0000

    well i done it

--- starter_code_ruby_functions/functions.rb
+++ starter_code_ruby_functions/functions.rb
@@ -83,6 +83,18 @@ end
 
 def fizz_buzz number 
 
+	if number % 3 == 0 && number % 5 == 0
+		 "FizzBuzz"
+	
+	elsif number % 5 === 0
+		"Buzz"
+
+	elsif number % 3 === 0
+		"Fizz"
+	else 
+		number
+	end
+
 
 end
 
