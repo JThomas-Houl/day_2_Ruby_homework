2b117a49833f91dc021bc677bced84aca8f9326d
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-03-07 09:32:09 +0000

    just the last one

--- starter_code_ruby_functions/functions.rb
+++ starter_code_ruby_functions/functions.rb
@@ -70,7 +70,8 @@ end
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
-def convert_array_to_a_hash 
+def convert_array_to_a_hash to_Hash
+	to_Hash.each_slice(2).to_h
 
 end
 
